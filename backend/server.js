6cd930a5f3c973176abc837c0eca150145d5bd0c
Flora Noiret <flo.n@hotmail.fr>
2023-08-24 14:08:14 +0200

    Distinguish new items from quantity change in cart

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,23 @@ app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
+// Product page
 app.get('/api/products/slug/:slug', (req, res) => {
   const product = data.products.find((x) => x.slug === req.params.slug);
   if (product) {
     res.send(product);
   } else {
     res.status(404).send({ message: 'Produit Non Trouvé' });
+  }
+});
+
+// Cart
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
   }
 });
 
