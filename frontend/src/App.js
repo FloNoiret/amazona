a388a556416ac340d33c3b891c26e9d4a538813f
Flora Noiret <flo.n@hotmail.fr>
2023-08-28 18:58:57 +0200

    Complete Sign In screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,19 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import { useContext } from "react";
 import { Store } from "./Store";
-import CartScreen from './screens/CartScreen';
+import CartScreen from "./screens/CartScreen";
 import SigninScreen from "./screens/SignInScreen";
 
 function App() {
-  // cart React Context
-  const { state } = useContext(Store);
-  const { cart } = state;
+  // cart & sign in React Context
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
+
+  // Signout
+  const signoutHandler = () => {
+    ctxDispatch({ type: "USER_SIGNOUT" });
+    localStorage.removeItem("userInfo");
+  };
 
   return (
     <BrowserRouter>
@@ -17,10 +23,40 @@ function App() {
         <header>
           <Link to="/"> Amazona</Link>
           <nav>
-            <Link to="/cart" className="cart-icone">
-             <div>Panier</div> {cart.cartItems.length > 0 && <div className="cart-number">{cart.cartItems.reduce((a, c) => a + c.quantity, 0)}</div>}
+            <Link to="/cart" className="cart-header">
+              <div>Panier</div>
+              {cart.cartItems.length > 0 && (
+                <div className="cart-number">
+                  {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                </div>
+              )}
             </Link>
-          </nav>
+            </nav>
+          <div className="sign-header">
+            {userInfo ? (
+              <div title={userInfo.name}>
+                <Link to="/profile">
+                  <div>User Profile</div>
+                </Link>
+                <Link to="/orderhistory">
+                  <div>Order History</div>
+                </Link>
+                <hr />
+                <Link
+                  className="btn-gold"
+                  to="#signout"
+                  onClick={signoutHandler}
+                >
+                  Sign Out
+                </Link>
+              </div>
+            ) : (
+              <Link className="btn-gold" to="/signin">
+                Sign In
+              </Link>
+            )}
+          </div>
+          
         </header>
         <main>
           <Routes>
--- frontend/src/screens/SignInScreen.js
+++ frontend/src/screens/SignInScreen.js
@@ -1,26 +1,71 @@
-import { Link, useLocation } from 'react-router-dom';
+import Axios from "axios";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Store } from "../Store";
 
 export default function SigninScreen() {
   const { search } = useLocation();
-  const redirectInUrl = new URLSearchParams(search).get('redirect');
-  const redirect = redirectInUrl ? redirectInUrl : '/';
+  const redirectInUrl = new URLSearchParams(search).get("redirect");
+  const redirect = redirectInUrl ? redirectInUrl : "/";
+  const navigate = useNavigate();
+
+  // Signin button action
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { userInfo } = state;
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await Axios.post("/api/users/signin", {
+        email,
+        password,
+      });
+      ctxDispatch({ type: "USER_SIGNIN", payload: data });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      navigate(redirect || "/");
+    } catch (err) {
+      alert('Identifiant ou Mot de passe invalide');
+    }
+  };
+
+  // Use Effect to redirect user to home page if try to access signin page while being connected. 
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
+
   return (
     <div className="signin-page">
       <h1>Se Connecter</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="signin-item" controlId="email">
           <label>Email</label>
-          <input className="signin-input" type="email" required />
+          <input
+            className="signin-input"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
         </div>
         <div className="signin-item" controlId="password">
           <label>Mot de Passe</label>
-          <input className="signin-input" type="password" required />
+          <input
+            className="signin-input"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
         </div>
         <div className="signin-item">
-          <button type="submit" className='btn-gold'>Se Connecter</button>
+          <button type="submit" className="btn-gold">
+            Se Connecter
+          </button>
         </div>
         <div className="signin-item">
-          Nouveau client?{' '}
+          Nouveau client?{" "}
           <Link to={`/signup?redirect=${redirect}`}>Créer un compte</Link>
         </div>
       </form>
