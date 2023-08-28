011593e165045b0de469f318d728dbb11a7e0cd9
Flora Noiret <flo.n@hotmail.fr>
2023-08-28 16:06:36 +0200

    Creation of Database + Product & User Sample

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import Product from '../models/productModel.js';
+
+const productRouter = express.Router();
+
+productRouter.get('/', async (req, res) => {
+  const products = await Product.find();
+  res.send(products);
+});
+
+//Product
+productRouter.get('/slug/:slug', async (req, res) => {
+  const product = await Product.findOne({ slug: { $eq: req.params.slug } });
+
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+
+//Cart
+productRouter.get('/:id', async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+
+export default productRouter;
