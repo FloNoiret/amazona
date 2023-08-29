6cdc8b03d2e23d20118811b1834b008e27f10d1e
Flora Noiret <flo.n@hotmail.fr>
2023-08-29 21:58:29 +0200

    Implement SearchBox and Categories nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { getError } from "./utils";
+import axios from "axios";
 
 // DATA
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Store } from "./Store";
 // WEBSITE PAGE
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import SigninScreen from "./screens/SignInScreen";
-import ShippingAddressScreen from './screens/ShippingAddressScreen';
+import ShippingAddressScreen from "./screens/ShippingAddressScreen";
 import SignupScreen from "./screens/SignUpScreen";
 import PaymentMethodScreen from "./screens/PaymentMethodScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
-import OrderScreen from './screens/OrderScreen';
-import OrderHistoryScreen from './screens/OrderHistoryScreen';
-import ProfileScreen from './screens/ProfileScreen';
+import OrderScreen from "./screens/OrderScreen";
+import OrderHistoryScreen from "./screens/OrderHistoryScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import SearchScreen from './screens/SearchScreen';
+import SearchBox from "./components/Searchbox";
 
 function App() {
   // cart & sign in React Context
@@ -25,50 +29,97 @@ function App() {
   const signoutHandler = () => {
     ctxDispatch({ type: "USER_SIGNOUT" });
     localStorage.removeItem("userInfo");
-    localStorage.removeItem('shippingAddress');
-    localStorage.removeItem('paymentMethod');
-    window.location.href = '/signin';
+    localStorage.removeItem("shippingAddress");
+    localStorage.removeItem("paymentMethod");
+    window.location.href = "/signin";
   };
+
+  // Side Bar
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const { data } = await axios.get(`/api/products/categories`);
+        setCategories(data);
+      } catch (err) {
+        alert(getError(err));
+      }
+    };
+    fetchCategories();
+  }, []);
 
   return (
     <BrowserRouter>
       <div className="App">
         <header>
-          <Link to="/"> Amazona</Link>
+          <Link to="/" className="logo-nav"> Amazona</Link>
           <nav>
-            <Link to="/cart" className="cart-header">
-              <div>Panier</div>
-              {cart.cartItems.length > 0 && (
-                <div className="cart-number">
-                  {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
-                </div>
-              )}
-            </Link>
-            </nav>
-          <div className="sign-header">
-            {userInfo ? (
-              <div title={userInfo.name} className="sign-header">
-                <Link to="/profile" >
-                  <div>Mon Compte</div>
-                </Link>
-                <Link to="/orderhistory">
-                  <div>Commandes Passées</div>
-                </Link>
-                <Link
-                  className="btn-gold"
-                  to="#signout"
-                  onClick={signoutHandler}
-                >
-                  Sign Out
-                </Link>
+            <div className="search-nav">
+              <div>
+                <ul>
+                  <legend>Categories</legend>
+                  {categories.map((category) => (
+                    <li className="category-nav" key={category}>
+                      <Link to={`/search?category=${category}`}>
+                        {category}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
               </div>
-            ) : (
-              <Link className="btn-gold" to="/signin">
-                Se Connecter
-              </Link>
-            )}
-          </div>
-          
+              <div>
+                <SearchBox />
+              </div>
+            </div>
+
+            <div>
+              <ul className="right-nav">
+                <div>
+                  <li className="sign-header">
+                    <Link to="/cart" className="cart-header">
+                      <p>Panier</p>
+                      {cart.cartItems.length > 0 && (
+                        <div className="cart-number">
+                          {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                        </div>
+                      )}
+                    </Link>
+                  </li>
+                </div>
+
+                <div>
+                  {userInfo ? (
+                    <div title={userInfo.name}  className="right-nav" >
+                      <li className="sign-header">
+                        <Link to="/profile">
+                          <p>Mon Compte</p>
+                        </Link>
+                      </li>
+                      <li className="sign-header">
+                        <Link to="/orderhistory">
+                          <p>Commandes Passées</p>
+                        </Link>
+                      </li>
+                      <li className="sign-header">
+                        <Link
+                          className="btn-gold"
+                          to="#signout"
+                          onClick={signoutHandler}
+                        >
+                          Sign Out
+                        </Link>
+                      </li>
+                    </div>
+                  ) : (
+                    <Link className="btn-gold" to="/signin">
+                      Se Connecter
+                    </Link>
+                  )}
+                </div>
+              </ul>
+            </div>
+          </nav>
         </header>
         <main>
           <Routes>
@@ -76,19 +127,17 @@ function App() {
             <Route path="/product/:slug" element={<ProductScreen />} />
             <Route path="/cart" element={<CartScreen />} />
             <Route path="/signin" element={<SigninScreen />} />
+            <Route path="/shipping" element={<ShippingAddressScreen />}></Route>
+            <Route path="/signup" element={<SignupScreen />} />
+            <Route path="/payment" element={<PaymentMethodScreen />}></Route>
+            <Route path="/placeorder" element={<PlaceOrderScreen />} />
+            <Route path="/order/:id" element={<OrderScreen />}></Route>
+            <Route path="/search" element={<SearchScreen />} />
             <Route
-                path="/shipping"
-                element={<ShippingAddressScreen />}
-              ></Route>
-                <Route path="/signup" element={<SignupScreen />} />
-                <Route path="/payment" element={<PaymentMethodScreen />}></Route>
-                <Route path="/placeorder" element={<PlaceOrderScreen />} />
-                <Route path="/order/:id" element={<OrderScreen />}></Route>
-                <Route
-                path="/orderhistory"
-                element={<OrderHistoryScreen />}
-              ></Route>
-              <Route path="/profile" element={<ProfileScreen />} />
+              path="/orderhistory"
+              element={<OrderHistoryScreen />}
+            ></Route>
+            <Route path="/profile" element={<ProfileScreen />} />
           </Routes>
         </main>
       </div>
